18c3cf01f8ef306d45ced04a9d4c366cff8c0fcc
Anil Deepak Talathoti <78140366+anil-deepak@users.noreply.github.com>
2021-03-04 19:49:43 -0500

    binding Nave.vue and Footer.vue to App.vue and default exporting as components

---
+++ src/components/partials/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Fitness</strong> by <a href="/">Anil Deepak T</a>. @2021
+            <a href="/">Exercise</a>
+          </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+      name: 'Footer',
+      components:{
+        
+      },
+      data(){
+        return{
+          
+        }
+      },
+      computed:{
+      },
+      methods: {
+      }
+    }
+</script>
+<style lang="scss" scoped>
+  .footer {
+    padding:40px;
+  }
+</style>
