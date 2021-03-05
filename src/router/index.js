6e3abc5fbd4d9a1bff4e0ade945041e0d4696b8e
Anil Deepak Talathoti <78140366+anil-deepak@users.noreply.github.com>
2021-03-04 20:45:01 -0500

    fixing import Signup from views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login'
-
-
+import Signup from '../views/Signup'
 const routes = [
   { path: '/', name: 'Home', component: Home,
   },
