0be4bf48ad48cd2bc0d29d42ec8e4fa44ec92881
Anil Deepak Talathoti <78140366+anil-deepak@users.noreply.github.com>
2021-03-14 12:53:05 -0400

    added tools view

---
+++ src/views/Tools.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column ">
+        <date />
+      </div>
+      <div class="column">
+        <weather />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Date from "../components/Date.vue";
+import Weather from "../components/Quote.vue";
+export default {
+  components: {
+    Date,
+    Weather,
+  },
+  Date,
+};
+</script>
