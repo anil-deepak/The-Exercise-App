5c73f77dbd0389625540ee7f520e9f2f4b0bdcf8
Anil Deepak Talathoti <78140366+anil-deepak@users.noreply.github.com>
2021-03-14 12:37:50 -0400

    added session storage in login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="section">
     <div class="container ">
-      
       <div class="panel">
         <p class="panel-heading">Sign In</p>
         <div class="panel-block">
           <div class="field">
             <label class="label">Username</label>
             <div class="control has-icons-left">
-               <input
+              <input
                 type="text"
-                :class="`input ${v$.credentials.username.$invalid ? 'is-danger' : ''}`"
+                class="input"
                 placeholder="Your username"
                 v-model.trim="credentials.username"
+                required
               />
               <span class="icon is-small is-left">
                 <i class="fas fa-user"></i>
               </span>
-              <p v-if="!v$.credentials.username.required" class="help is-danger">Field is required</p>
             </div>
           </div>
         </div>
@@ -25,101 +24,97 @@
           <div class="field">
             <label class="label">Password</label>
             <div class="control has-icons-left">
-             <input
+              <input
                 type="password"
-                :class="`input ${v$.credentials.password.$invalid ? 'is-danger' : ''}`"
+                class="input"
                 placeholder="Your password"
                 v-model.trim="credentials.password"
+                required
               />
               <span class="icon is-small is-left">
                 <i class="fas fa-lock"></i>
               </span>
-              <p v-if="!v$.credentials.password.required" class="help is-danger">Field is required</p>
             </div>
           </div>
         </div>
         <div class="panel-block">
+          <p class="help is-danger">
+            {{ error }}
+          </p>
+        </div>
+        <div class="panel-block">
           <div class="field">
             <div class="control">
-               <a
-                class="button is-primary w-100"
-                @click="loginBtnClicked()"
-              >Login</a>
-              <p v-if="wrongCredentials" class="help is-danger">Username or password are wrong</p>
+              <a class="button is-primary w-100" @click="loginBtnClicked()"
+                >Login</a
+              >
             </div>
           </div>
         </div>
       </div>
-      
     </div>
   </div>
 </template>
 <script>
-import useVuelidate from '@vuelidate/core'
-import { required } from '@vuelidate/validators'
-import Users from '@/models/Users.js'
-import router from '@/router/index.js'
+
+import router from "@/router/index.js";
 export default {
-   name: "SignIn",
+  name: "SignIn",
   data() {
-    return{
-      credentials:{
-        username: null,
-        password: null
+    return {
+      credentials: {
+        username: "",
+        password: "",
       },
-      wrongCredentials: false
+      error: "",
     };
   },
-  setup () {
-    return { v$: useVuelidate() }
-  },
-  validations() {
-    return {
-      credentials:{
-        username:{
-          required
-        },
-        password:{
-          required
-        }
-      }
+
+  mounted() {
+    if (sessionStorage.isLoggedIn) {
+      this.$router.push("/");
     }
   },
   methods: {
-    
-    async loginBtnClicked() {
-      if (!this.v$.credentials.$invalid) {
-        var users = Users;
-        const user = users.find(x => x.username == this.credentials.username)
-        if(user != undefined && user != '') {
-          if(user.password == this.credentials.password) {
-            sessionStorage.setItem('user', JSON.stringify(user))
-            sessionStorage.setItem('isLoggedIn', true)
-            this.wrongCredentials = true;
-            router.push('/')
-          }else{
-            this.wrongCredentials = false;
-            this.resetForm();
-          }
-        }else{
-          this.wrongCredentials = false;
-          this.resetForm();
-        }
-
-        
-        
+    validateFields() {
+      // email
+      if (this.credentials.username.length === 0) {
+        this.error = "Username is required";
+        return false;
       }
-      
+
+      // password
+      if (this.credentials.password.length === 0) {
+        this.error = "Password is required";
+        return false;
+      }
+
+      return true;
     },
-    resetForm(){
+    loginBtnClicked() {
+      if (this.validateFields()) {
+        let users = JSON.parse(sessionStorage.getItem("users"));
+        const user = users.find((x) => x.username == this.credentials.username);
+        if (user != undefined && user != "") {
+          if (user.password == this.credentials.password) {
+            sessionStorage.setItem("user", JSON.stringify(user));
+            sessionStorage.setItem("isLoggedIn", true);
+            this.wrongCredentials = true;
+            this.resetForm();
+            router.push("/");
+          }
+        }
+        this.resetForm();
+      }
+    },
+    resetForm() {
       this.credentials.username = null;
       this.credentials.password = null;
-    }
-  }
+    },
+  },
 };
 </script>
 <style>
-
 .w-100,
 .field {
   width: 100%;
